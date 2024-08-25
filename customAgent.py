20be0a61e7cf9449e8dcf33ef9c8ad4ead2d08fd
Gowri <gowriks12@gmail.com>
2024-08-25 15:26:30 -0400

    Cleaning

--- customAgent.py
+++ customAgent.py
@@ -1,7 +1,4 @@
-import pandas as pd
 from dotenv import load_dotenv
-# from llama_index.experimental.query_engine import PandasQueryEngine
-# from customPrompts.viz_engine_prompts import visualizer_prompt, instruction_str, response_synthesis_prompt_str
 from customPrompts.agent_prompts import context, new_agent_prompt
 from customTools.titanic_info_engine import titanic_info_tool
 from customTools.WiKi_engine import Wikipedia
@@ -10,10 +7,6 @@ from customTools.data_visualizer import data_visualizer
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 from generateReport import createDoc
-from utilFuncs import *
-from customTools.note_engine import note_engine
-import datetime
-import os
 load_dotenv()
 
 
@@ -23,24 +16,14 @@ llm = OpenAI(model="gpt-3.5-turbo-instruct")
 agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
 # Updating the prompt
 agent.update_prompts({'agent_worker:system_prompt':new_agent_prompt})
-# print(agent)
+
 report = ""
 
 while(prompt := input("Enter a prompt (q to quit): ")) != "q":
     result = agent.query(prompt)
     report += result.__str__() + '\n'
     print(result)
-    # note_engine.call(result)
 
 createDoc(text=report)
-# images_generated = get_plots_for_report()
-# print(images_generated)
-# for image in images_generated:
-#     report += str(image) + '\n'
-#
-# report_name = get_report_name()
-# note_file = os.path.join("Reports", report_name)
 
-# save_note(report)
-# note_engine.call(report)
 
