18158f5df79a0ddbb00501c1b7903074ea3e7f67
Gowri <gowriks12@gmail.com>
2024-08-26 11:42:41 -0400

    UI Development

--- customTools/titanic_info_engine.py
+++ customTools/titanic_info_engine.py
@@ -5,17 +5,17 @@ from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core import StorageContext, load_index_from_storage
 
 
-def get_index(data, index_name):
-    index = None
-    if not os.path.exists(index_name):
-        print("building index", index_name)
-        index = VectorStoreIndex.from_documents(data, show_progress=True)
-        index.storage_context.persist(persist_dir=index_name)
-    else:
-        index = load_index_from_storage(
-            StorageContext.from_defaults(persist_dir=index_name)
-        )
-    return index
+# def get_index(data, index_name):
+#     index = None
+#     if not os.path.exists(index_name):
+#         print("building index", index_name)
+#         index = VectorStoreIndex.from_documents(data, show_progress=True)
+#         index.storage_context.persist(persist_dir=index_name)
+#     else:
+#         index = load_index_from_storage(
+#             StorageContext.from_defaults(persist_dir=index_name)
+#         )
+#     return index
 
 def get_index(index_path):
     index = None
