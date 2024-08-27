cc44838283fd13e263094e1a69a50e1f1f303df1
Gowri <82420256+gowriks12@users.noreply.github.com>
2024-08-26 22:08:48 -0400

    Gen agent (#4)
    * User Data Submission Dev
    
    * Results

--- customAgent.py
+++ customAgent.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv, find_dotenv
 from customPrompts.agent_prompts import context, new_agent_prompt
-from customTools.titanic_info_engine import titanic_info_tool
+from customTools.info_engine import get_titanic_info_tool
 from customTools.WiKi_engine import Wikipedia
-from customTools.data_answering import data_answering
-from customTools.data_visualizer import data_visualizer
+from customTools.data_answering import get_data_answering_tool, get_titanic_data_answering_tool
+from customTools.data_visualizer import get_data_visualizer_tool, get_titanic_data_visualizer_tool
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 from generateReport import createDoc
@@ -11,17 +11,33 @@ from generateReport import createDoc
 import os
 load_dotenv(find_dotenv())
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-# print(OPENAI_API_KEY)
-tools = [data_answering, data_visualizer, titanic_info_tool, Wikipedia]
+def get_agent(df):
+    # print(data_path)
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    # print(OPENAI_API_KEY)
+    # tools = [data_answering, data_visualizer, titanic_info_tool, Wikipedia]
+    tools = [get_data_answering_tool(df),get_data_visualizer_tool(df),Wikipedia]
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_API_KEY)
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_API_KEY)
+    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+    # Updating the prompt
+    agent.update_prompts({'agent_worker:system_prompt':new_agent_prompt})
+    return agent
 
-agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
-# Updating the prompt
-agent.update_prompts({'agent_worker:system_prompt':new_agent_prompt})
+def get_titanic_agent():
+    data_path = "data/titanic_processed.csv"
+    pdf_path = "data/titanic_info.pdf"
+    print(data_path, pdf_path)
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    # print(OPENAI_API_KEY)
+    # tools = [data_answering, data_visualizer, titanic_info_tool, Wikipedia]
+    tools = [get_titanic_data_answering_tool(data_path),get_titanic_data_visualizer_tool(data_path),get_titanic_info_tool(),Wikipedia]
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", openai_api_key=OPENAI_API_KEY)
 
-
+    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+    # Updating the prompt
+    agent.update_prompts({'agent_worker:system_prompt':new_agent_prompt})
+    return agent
 # report = ""
 
 # while(prompt := input("Enter a prompt (q to quit): ")) != "q":
--- customTools/data_visualizer.py
+++ customTools/data_visualizer.py
@@ -6,18 +6,38 @@ from customPrompts.viz_engine_prompts import visualizer_prompt, instruction_str,
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 
 load_dotenv()
+def get_data_visualizer_tool(df):
+    # print("data_path",data_path)
+    # df = pd.read_csv(data_path)
 
-df = pd.read_csv("data/titanic_processed.csv")
+    df_query_engine = PandasQueryEngine(df = df, verbose=True, instruction_str=instruction_str) #specifying the query engine for RAG
+    df_query_engine.update_prompts({"pandas_prompt": visualizer_prompt, "response_synthesis_prompt":response_synthesis_prompt_str})
 
-df_query_engine = PandasQueryEngine(df = df, verbose=True, instruction_str=instruction_str) #specifying the query engine for RAG
-df_query_engine.update_prompts({"pandas_prompt": visualizer_prompt, "response_synthesis_prompt":response_synthesis_prompt_str})
+    # df_query_engine.query("plot a histogram of age in titanic")
+    # df_query_engine.query("Create a bar plot representing ratio of male and female that survived titanic disaster")
+    # df_query_engine.query("plot a bar graph depicting correlation between survived and age, divide the age groups into buckets of 10 years and explain the plot")
 
-# df_query_engine.query("plot a histogram of age in titanic")
-# df_query_engine.query("Create a bar plot representing ratio of male and female that survived titanic disaster")
-# df_query_engine.query("plot a bar graph depicting correlation between survived and age, divide the age groups into buckets of 10 years and explain the plot")
+    data_visualizer = QueryEngineTool(query_engine=df_query_engine, metadata=ToolMetadata(
+        name="titanic_data_visualizer",
+        description="Use this tool to create plots and data visualizations for the user provided data",
+        ),
+    )
+    return data_visualizer
 
-data_visualizer = QueryEngineTool(query_engine=df_query_engine, metadata=ToolMetadata(
-    name="titanic_data_visualizer",
-    description="This tool helps in titanic passenger data visualization by creating plots on titanic passenger data",
-    ),
-)
+
+def get_titanic_data_visualizer_tool(data_path= "data/titanic_processed.csv"):
+    df = pd.read_csv(data_path)
+
+    df_query_engine = PandasQueryEngine(df = df, verbose=True, instruction_str=instruction_str) #specifying the query engine for RAG
+    df_query_engine.update_prompts({"pandas_prompt": visualizer_prompt, "response_synthesis_prompt":response_synthesis_prompt_str})
+
+    # df_query_engine.query("plot a histogram of age in titanic")
+    # df_query_engine.query("Create a bar plot representing ratio of male and female that survived titanic disaster")
+    # df_query_engine.query("plot a bar graph depicting correlation between survived and age, divide the age groups into buckets of 10 years and explain the plot")
+
+    data_visualizer = QueryEngineTool(query_engine=df_query_engine, metadata=ToolMetadata(
+        name="titanic_data_visualizer",
+        description="This tool helps in titanic passenger data visualization by creating plots on titanic passenger data",
+        ),
+    )
+    return data_visualizer
